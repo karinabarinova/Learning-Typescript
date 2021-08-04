8a4bf194a10ecf005ce5d18981a1136aa9e94ed5
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-08-04 15:24:43 +0300

    Work with objects and tuples

---
+++ ts-2/using-ts.ts
@@ -0,0 +1,11 @@
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
+  name: "Karina",
+  age: 105,
+  hobbies: ["Sports", "Cooking"],
+  role: [1, "author"]
+};
