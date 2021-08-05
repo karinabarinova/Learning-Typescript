69859e333bfb2f12289210437d3ef94b366fe019
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-08-05 17:17:37 +0300

    rest params

--- ts-13/src/app.ts
+++ ts-13/src/app.ts
@@ -1,15 +1,9 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-    const result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    } else {
-        return result;
-    }
+//rest params
+
+const add = (...params: number[]) => {
+    return params.reduce((cur, value) => {
+        return cur + value;
+    }, 0)
 }
 
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result is: '
-
-add(number1, number2, printResult, resultPhrase);
+console.log(add(1, 2, 3, 4, 5, 6, 7, 9));
