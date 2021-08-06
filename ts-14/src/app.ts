cf8135a81aa8e5292cd1b904bed1c0e564e39f06
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-08-06 10:47:30 +0300

    static methods & properties

--- ts-14/src/app.ts
+++ ts-14/src/app.ts
@@ -1,7 +1,20 @@
 class Department {
     protected employees: string[] = [];
+    static fiscalYear = 2021;
 
     constructor(private readonly id: string, public name: string) {}
+
+    static createEmployee(name: string) {
+        return {name};
+    }
+
+    static changeFiscalYear(year: number) {
+        if (year) {
+            this.fiscalYear = year;
+            return this.fiscalYear;
+        }
+        throw new Error('Please provide a valid value');
+    }
 
     changeName(n: string) {
         this.name = n;
@@ -66,6 +79,10 @@ class AccountingDepartment extends Department {
     }
 }
 
+const employee1 = Department.createEmployee('Inna');
+console.log(employee1)
+console.log(Department.fiscalYear);
+
 const it = new ITDepartment('d1', ['Karina']);
 it.describe();
 it.addEmployee("Karl");
@@ -81,3 +98,5 @@ accounting.addEmployee('Menu');
 accounting.printEmployeeInformation();
 accounting.mostRecentReport = 'Year End Report';
 console.log(accounting.mostRecentReport);
+Department.changeFiscalYear(2022);
+console.log(Department.fiscalYear);
