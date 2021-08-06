de85f5852ecafeca1b6eb28d362e92f7e187bcfd
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-08-06 10:22:34 +0300

    Inheritance

--- ts-14/src/app.ts
+++ ts-14/src/app.ts
@@ -21,9 +21,34 @@ class Department {
     }
 }
 
-const accounting = new Department('d1', 'Accounting');
-accounting.describe();
-accounting.addEmployee("Karl");
-accounting.addEmployee("Max");
-accounting.printEmployeeInformation();
+class ITDepartment extends Department {
+    constructor(id: string, public admins: string[]) {
+        super(id, 'IT');
+        this.admins = admins;
+    }
+}
+
+class AccountingDepartment extends Department {
+    constructor(id: string, public reports: string[]) {
+        super(id, 'Accounting');
+    }
+
+    addReport(text: string) {
+        this.reports.push(text);
+    }
+
+    printReports() {
+        console.log(this.reports);
+    }
+}
+
+const it = new ITDepartment('d1', ['Karina']);
+it.describe();
+it.addEmployee("Karl");
+it.addEmployee("Max");
+it.printEmployeeInformation();
+console.log(it);
+const accounting = new AccountingDepartment('d2', []);
+accounting.addReport('Something went wrong...');
+accounting.printReports();
 
