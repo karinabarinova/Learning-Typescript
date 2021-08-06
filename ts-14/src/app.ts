91215e5b414d8cf50f56d15089afbf60e47080ba
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-08-06 10:26:21 +0300

    overriding props & the 'protected' modifier

--- ts-14/src/app.ts
+++ ts-14/src/app.ts
@@ -1,5 +1,5 @@
 class Department {
-    private employees: string[] = [];
+    protected employees: string[] = [];
 
     constructor(private readonly id: string, public name: string) {}
 
@@ -33,6 +33,13 @@ class AccountingDepartment extends Department {
         super(id, 'Accounting');
     }
 
+    addEmployee(name: string) {
+        if (name === 'Karina') {
+            return;
+        }
+        this.employees.push(name);
+    }
+
     addReport(text: string) {
         this.reports.push(text);
     }
@@ -48,7 +55,10 @@ it.addEmployee("Karl");
 it.addEmployee("Max");
 it.printEmployeeInformation();
 console.log(it);
+
 const accounting = new AccountingDepartment('d2', []);
 accounting.addReport('Something went wrong...');
 accounting.printReports();
-
+accounting.addEmployee('Karina');
+accounting.addEmployee('Menu');
+accounting.printEmployeeInformation();
