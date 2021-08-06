a97e043d53bf368202fab00b2b7abdd7fdece445
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-08-06 11:48:04 +0300

    Optional Parameters & Properties

--- ts-14/src/app.ts
+++ ts-14/src/app.ts
@@ -11,6 +11,8 @@ add = (n1: number, n2: number) => {
 
 interface Named {
     readonly name: string;
+    outputName?: string; //optional prop
+    optionalMethod?(data: string): void; //optional method
 }
 
 interface Greetable extends Named {    
@@ -19,8 +21,12 @@ interface Greetable extends Named {
 
 class Person implements Greetable {
     name: string;
+    lastName?: string; //optional prop on class
     age = 30;
-    constructor(name: string) {
+    constructor(name: string, lastName?: string) {
+        if (lastName) {
+            this.lastName = lastName;
+        }
         this.name = name;
     }
 
