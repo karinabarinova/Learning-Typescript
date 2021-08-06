421415cb973519531707e1ef3c4457be88c8569c
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-08-06 10:35:40 +0300

    getters & setters

--- ts-14/src/app.ts
+++ ts-14/src/app.ts
@@ -29,8 +29,24 @@ class ITDepartment extends Department {
 }
 
 class AccountingDepartment extends Department {
+    private lastReport: string;
     constructor(id: string, public reports: string[]) {
         super(id, 'Accounting');
+        this.lastReport = reports[0];
+    }
+
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        throw new Error('No report found');
+    }
+
+    set mostRecentReport(value: string) {
+        if (!value) {
+            throw new Error('Please pass in a valid value');
+        }
+        this.addReport(value);
     }
 
     addEmployee(name: string) {
@@ -42,6 +58,7 @@ class AccountingDepartment extends Department {
 
     addReport(text: string) {
         this.reports.push(text);
+        this.lastReport = text;
     }
 
     printReports() {
@@ -62,3 +79,5 @@ accounting.printReports();
 accounting.addEmployee('Karina');
 accounting.addEmployee('Menu');
 accounting.printEmployeeInformation();
+accounting.mostRecentReport = 'Year End Report';
+console.log(accounting.mostRecentReport);
