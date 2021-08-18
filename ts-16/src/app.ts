3ac16583d8eefd2a224787b97cad59a7fb0d8ce8
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-08-18 16:34:49 +0300

    Update app.ts

--- ts-16/src/app.ts
+++ ts-16/src/app.ts
@@ -69,7 +69,10 @@ const v2 = new Truck();
 function useVehicle(vehicle: Vehicle) {
     vehicle.drive();
 
-    if ('loadCargo' in vehicle) {
+    // if ('loadCargo' in vehicle) {
+    //     vehicle.loadCargo(1000);
+    // }
+    if (vehicle instanceof Truck) {
         vehicle.loadCargo(1000);
     }
 }
