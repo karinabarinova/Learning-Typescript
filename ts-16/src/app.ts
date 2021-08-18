2557e030bd464cf8717e7a8309b362c33a67fef4
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-08-18 16:34:24 +0300

    type guards

---
+++ ts-16/src/app.ts
@@ -0,0 +1,75 @@
+// Type Guards
+
+type Admin = {
+    name: string;
+    privileges: string[];
+}
+
+type Employee = {
+    name: string;
+    startDate: Date;
+}
+
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+    name: "Karl",
+    privileges: ["read", "write"],
+    startDate: new Date()
+}
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+    console.log('Name: ' + emp.name);
+    if ('privileges' in emp) {
+        console.log('Priviledges: ', emp.privileges);
+    }
+
+    if ('startDate' in emp) {
+        console.log('startDate: ', emp.startDate);
+    }
+}
+
+printEmployeeInformation(e1);
+
+class Car {
+    drive() {
+        console.log('Driving...')
+    }
+}
+
+class Truck {
+    drive() {
+        console.log('Driving truck...')
+    }
+
+    loadCargo(amount: number) {
+        console.log("Loading cargo " + amount )
+    }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+    vehicle.drive();
+
+    if ('loadCargo' in vehicle) {
+        vehicle.loadCargo(1000);
+    }
+}
